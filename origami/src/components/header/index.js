9144c5d6882017a4ff12f73a2dcd0e4a1e1a9f4f
martinmalamov <47690979+martinmalamov@users.noreply.github.com>
2020-09-15 23:35:54 +0300

    feat: add dynamic nav

--- origami/src/components/header/index.js
+++ origami/src/components/header/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Link from '../link/index'
 import styles from './index.module.css'
 import logo from '../../images/origam.jpg'
+import getNavigation from '../../utils/navigation'
 
 const Header = () => {
+    const links = getNavigation()
     return (
         <header className={styles.navigation}>
             <img className={styles.logo} src={logo} />
-            <Link href="#" title="Going to 1" type="header" />
-            <Link href="#" title="Going to 2" type="header" />
-            <Link href="#" title="Going to 3" type="header" />
-            <Link href="#" title="Going to 4" type="header" />
-            <Link href="#" title="Going to 5" type="header" />
+            {
+                links.map(navElement => {
+                    return (
+                        <Link href={navElement.link} title={navElement.title} type="header" />
+                    )
+                })
+            }
         </header>
     )
 }
